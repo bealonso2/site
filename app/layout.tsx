12c81b156591974eafb11ddc58cbb80bc2af934a
Brian Alonso <alonsobrian2@gmail.com>
2024-08-18 12:19:19 -0400

    Maybe fixing navbar on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,32 +81,30 @@ export default function RootLayout({
             </label>
           </div>
           <div className="navbar-end md:hidden">
-            <div className="dropdown relative">
-              <ul className="menu menu-horizontal px-1">
-                <li>
-                  <details>
-                    <summary className="">
-                      {" "}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M4 6h16M4 12h8m-8 6h16"
-                        />
-                      </svg>
-                    </summary>
-                    <NavMenu className="dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box absolute right-0" />
-                  </details>
-                </li>
-              </ul>
-            </div>
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <details>
+                  <summary className="">
+                    {" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h8m-8 6h16"
+                      />
+                    </svg>
+                  </summary>
+                  <NavMenu className="dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box absolute right-0" />
+                </details>
+              </li>
+            </ul>
           </div>
         </header>
         {children}
