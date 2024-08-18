ab40f1511b91d4deef270ca53cd2eac706367c3c
Brian Alonso <alonsobrian2@gmail.com>
2024-08-18 17:46:25 -0400

    Fixing color themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import { ThemeProvider } from "@/components/layout/ThemeProvider";
+import { ThemeSwitcher } from "@/components/layout/ThemeSwitcher";
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Roboto_Flex } from "next/font/google";
-import { NavMenu } from "../components/NavMenu";
+import { NavMenu } from "../components/layout/NavMenu";
 import "./globals.css";
 
 const RobotoFlex = Roboto_Flex({
@@ -42,8 +44,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    // data-theme="dark"
     <html lang="en" className="bg-base-200">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var savedTheme = localStorage.getItem('theme');
+                  var prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+                  var theme = savedTheme || (prefersDark ? 'dark' : 'light');
+                  document.documentElement.setAttribute('data-theme', theme);
+                } catch (e) {}
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className={`${RobotoFlex.className} font-light`}>
+        <ThemeProvider />
         <header className="navbar px-4">
           <div className="navbar-start">
             <a href="/" className="btn btn-ghost text-xl">
@@ -54,32 +74,7 @@ export default function RootLayout({
             <NavMenu className="menu menu-horizontal px-1" />
           </div>
           <div className="flex-shrink md:navbar-end">
-            <label className="swap swap-rotate">
-              {/* this hidden checkbox controls the state */}
-              <input
-                type="checkbox"
-                className="theme-controller"
-                value="light"
-              />
-
-              {/* sun icon */}
-              <svg
-                className="swap-off fill-current w-10 h-10"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
-              </svg>
-
-              {/* moon icon */}
-              <svg
-                className="swap-on fill-current w-10 h-10"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
-              </svg>
-            </label>
+            <ThemeSwitcher />
           </div>
           <div className="navbar-end md:hidden">
             <ul className="menu menu-horizontal px-1">
