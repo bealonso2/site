814fa025b2fc7e3a66597a8b3fbcd748084c8e93
Brian Alonso <alonsobrian2@gmail.com>
2024-08-21 08:06:53 -0400

    Adding next sitemap
    Also deletes index.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: "https://www.balonso.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  // use this to exclude routes from the sitemap (i.e. a user dashboard). By default, NextJS app router metadata files are excluded (https://nextjs.org/docs/app/api-reference/file-conventions/metadata)
+  // exclude: ["/twitter-image.*", "/opengraph-image.*", "/icon.*"],
+};
