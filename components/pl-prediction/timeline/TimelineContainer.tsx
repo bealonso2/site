e8466512c10f1128befcafb07a49489e7a4e5617
Brian Alonso <alonsobrian2@gmail.com>
2025-03-03 07:49:17 -0500

    Revert "Add normalization for team placement in timeline data y axis"
    Reason: The "place" shown in the normalized y axis doesn't line up with what the user sees on the table UI.
    
    This reverts commit 299f1c94343c10bccd71df71ea42a58ba6c2ac2e.

--- components/pl-prediction/timeline/TimelineContainer.tsx
+++ components/pl-prediction/timeline/TimelineContainer.tsx
@@ -15,11 +15,6 @@ interface AvgFinishData {
   win_premier_league: number;
 }
 
-function normalizePlace(place: number, max: number, min: number) {
-  // Normalize the place to a value between 1 and 20
-  return ((place - min) / (max - min)) * 19 + 1;
-}
-
 export default function TimelineContainer({
   avgFinishData,
   seasonToDates,
@@ -78,22 +73,6 @@ export default function TimelineContainer({
       .map(({ team }) => team),
   );
 
-  // Find max and min place for each date/simulation uuid
-  const maxMinPlacePerUUID = Object.fromEntries(
-    Object.entries(datesAndUUIDs as Map<string, string>).map(([date, uuid]) => {
-      const places =
-        uuidToPositions[uuid]?.map(({ place }: { place: number }) => place) ||
-        [];
-      return [
-        uuid,
-        {
-          maxPlace: Math.max(...places),
-          minPlace: Math.min(...places),
-        },
-      ];
-    }),
-  );
-
   // Flatten data
   const flatData = Object.entries(datesAndUUIDs as Map<string, string>)
     .reverse()
@@ -110,11 +89,7 @@ export default function TimelineContainer({
             crest: string;
           }) => ({
             date: date,
-            place: normalizePlace(
-              place,
-              maxMinPlacePerUUID[uuid].maxPlace,
-              maxMinPlacePerUUID[uuid].minPlace,
-            ),
+            place: place,
             team: team,
             crest: crest,
           }),
@@ -130,10 +105,6 @@ export default function TimelineContainer({
         .map(({ date, place }) => ({ date, place })),
     ]),
   );
-
-  // Normalize
-
-  console.log("maxMinPlacePerUUID", maxMinPlacePerUUID);
 
   // Build the traces from the team data
   const dataPerTeam = Object.entries(teamData)
